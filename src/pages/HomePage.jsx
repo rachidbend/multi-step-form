be7c8a41f1ff58dddecb2c2364a283938ebdaec1
Rachid Bendaghor <rachidbendaghor@gmail.com>
2023-10-02 10:00:39 +0100

    now when you  enter the homepage it redirects you to the form

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,15 @@
-import { Outlet } from 'react-router';
+import { Outlet, useNavigate } from 'react-router';
 import SideBar from '../components/side-bar/SideBar';
 import styles from './HomePage.module.css';
+import { useEffect } from 'react';
 
 export default function HomePage() {
+  const navigate = useNavigate();
+
+  useEffect(function () {
+    navigate('/info');
+  }, []);
+
   return (
     <div className={styles.homepage}>
       <SideBar />
